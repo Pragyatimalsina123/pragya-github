930c7ec3189774e41d815f922a1d4484eb7a15f9
pragya <pragyatimalsina123@gmail.com>
2025-01-20 08:47:17 +0545

    task_decorator

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 print("this is our git tutorial")
 print("we are changing")
+
+#jamp install garne
+#sabai gareko laii github ma post garera aaune
---
+++ task_decorator.py
@@ -0,0 +1,24 @@
+from decoratorfile import access_decorator
+
+@access_decorator
+def name():
+    """This is the funcn to acess name"""
+    pass
+@access_decorator
+def edu():
+    """This is the function to acess edu"""
+    pass
+@access_decorator
+def age():
+    """This is the funcn to acess age"""
+    pass
+
+@access_decorator
+def skill():
+    """This is the funcn to acess skill"""
+    pass
+
+name()
+age()
+edu()
+skill()
